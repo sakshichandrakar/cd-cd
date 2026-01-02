d5ad433b8d1f082b53c2d20c1b47a46e6e79f509
sakshichandrakar <sakshichandrakar2911@gmail.com>
2026-01-02 23:00:36 +0530

    Update index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello CI/CD World  🚀" });
+  res.json({ message: "Hello CI/CD World 🚀" });
 });
 
 if (require.main === module) {
